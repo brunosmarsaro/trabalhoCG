7658571d749e10acc3b7db70977def7d2c3c55f1
ayerrsn <ayerrsn2@gmail.com>
2016-05-09 18:05:43 -0300

    teste2

---
+++ gLib.h
@@ -0,0 +1,17 @@
+//OpenGL Libs
+
+#if defined __APPLE__
+    #include <OpenGL/gl.h>
+    #include <OpenGL/glu.h>
+    #include <GLUT/glut.h>
+#else
+    #include <GL/gl.h>
+    #include <GL/glu.h>
+    #include <GL/glut.h>
+#endif
+
+//General Libs
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <cstdio>
